64bc6f78490f407cca9bb105eae38a1031362bd1
Nodetea <lk201330912@outlook.com>
2017-02-24 23:18:13 +0800

    添加项目文件。

---
+++ Test.Models/Goods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Test.Models
+{
+    public class Goods
+    {
+        [Key]
+        [Required]
+        [StringLength(20,ErrorMessage ="长度溢出")]
+        public string id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string category { get; set; }
+        public string reserve { get; set; }
+    }
+}
---
+++ Test.Web/Controllers/GoodsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Test.Models;
+using Test.Web.Models;
+
+namespace Test.Web.Controllers
+{
+    public class GoodsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Goods
+        public ActionResult Index()
+        {
+            return View(db.wares.ToList());
+        }
+
+        // GET: Goods/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Goods goods = db.wares.Find(id);
+            if (goods == null)
+            {
+                return HttpNotFound();
+            }
+            return View(goods);
+        }
+
+        // GET: Goods/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Goods/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,category,reserve")] Goods goods)
+        {
+            if (ModelState.IsValid)
+            {
+                db.wares.Add(goods);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(goods);
+        }
+
+        // GET: Goods/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Goods goods = db.wares.Find(id);
+            if (goods == null)
+            {
+                return HttpNotFound();
+            }
+            return View(goods);
+        }
+
+        // POST: Goods/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,category,reserve")] Goods goods)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(goods).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(goods);
+        }
+
+        // GET: Goods/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Goods goods = db.wares.Find(id);
+            if (goods == null)
+            {
+                return HttpNotFound();
+            }
+            return View(goods);
+        }
+
+        // POST: Goods/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Goods goods = db.wares.Find(id);
+            db.wares.Remove(goods);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
